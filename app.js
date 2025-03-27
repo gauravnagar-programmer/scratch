59364cd81a05752321d000851a4e2bdb656ae50a
Gaurav Nagar <gauravnagar365@gmail.com>
2025-03-27 07:39:11 +0530

    add new features

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-
 const cookieParser = require('cookie-parser')
 const path = require('path');
+const db = require('./config/mongoose-connection');
+const ownerRouter = require('./routers/ownerRouter');
+const usersRouter = require('./routers/usersRouter');
+const productsRouter = require('./routers/productsRouter');
 
 app.set('view engine','ejs');
 app.use(express.json());
@@ -10,8 +13,8 @@ app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"public")));
 
-app.get('/',(req,res)=>{
-  res.send('go')
-});
-
+app.use('/owners', ownerRouter)
+app.use('/users', usersRouter)
+app.use('/products', productsRouter)
+ 
 app.listen(3000);
